4c784424bf52ef6d36dafa3625af25276f0b32e0
Looknath <152136898+NeuralCoder1@users.noreply.github.com>
2026-01-25 21:33:28 +0530

    Refactor API_BASE declaration to template literal

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
 // ================= API CONFIG =================
-const API_BASE = window.location.origin + "/api";
+const API_BASE = `${window.location.origin}/api`;
 
 // ================= EVENT LISTENER =================
 document.getElementById("checkLeakBtn").addEventListener("click", () => {
